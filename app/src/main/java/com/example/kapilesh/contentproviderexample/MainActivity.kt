fdd80740af80350dbe5a6f647e643b6106824c8c
Rajput Kapilesh <rajputkapilesh@outlook.com>
2021-02-28 12:22:43 +0530

    Added basic Snackbar so app won't crash if the permission is denied & user clicks the FloatingActionButton

--- app/src/main/java/com/example/kapilesh/contentproviderexample/MainActivity.kt
+++ app/src/main/java/com/example/kapilesh/contentproviderexample/MainActivity.kt
@@ -13,13 +13,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 private const val TAG = "MainActivity"
 private const val REQUEST_CODE_READ_CONTACTS = 1
 
 class MainActivity : AppCompatActivity() {
     private var readGranted = false
-    val contact_names by lazy { findViewById<ListView>(R.id.contact_names) }
+    private val contactNames: ListView by lazy { findViewById<ListView>(R.id.contact_names) }
+    private val fab: FloatingActionButton by lazy { findViewById<FloatingActionButton>(R.id.fab) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,38 +44,45 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Log.d(TAG, "onCreate: onclick => starts")
-            val projection =
-                arrayOf(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY) // this is just the name of the column in the contacts table.
+        fab.setOnClickListener { view ->
+            if (readGranted) {
+                Log.d(TAG, "onCreate: onclick => starts")
+                val projection =
+                    arrayOf(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY) // this is just the name of the column in the contacts table.
 
-            // we get contentResolver from the activity using getContentResolver() but
-            // as this is kotlin we could use property directly without using getter.
-            val cursor = contentResolver.query(
-                ContactsContract.Contacts.CONTENT_URI,
-                projection,
-                null,
-                null,
-                ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
-            )
-            // params: 1 => URI = Identifies the source of our data
-            // 2 => stringArray holding the names of columns we want to retrive
-            // 3 => selection = filter to determine which rows to return (like a where clause in SQL statement).
-            // setting it to null means return all rows.
-            // 4 =>selectionArgs = array of values that will be used to replace placeholders in the selection string.
-            // ex: selection = "name = ?" & selectionArgs could contain single value "Frodo"
-            // 5 => sortOrder = string containing names of the fields you want the data sorted by. (ORDER BY clause)
+                // we get contentResolver from the activity using getContentResolver() but
+                // as this is kotlin we could use property directly without using getter.
+                val cursor = contentResolver.query(
+                    ContactsContract.Contacts.CONTENT_URI,
+                    projection,
+                    null,
+                    null,
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
+                )
+                // params: 1 => URI = Identifies the source of our data
+                // 2 => stringArray holding the names of columns we want to retrive
+                // 3 => selection = filter to determine which rows to return (like a where clause in SQL statement).
+                // setting it to null means return all rows.
+                // 4 =>selectionArgs = array of values that will be used to replace placeholders in the selection string.
+                // ex: selection = "name = ?" & selectionArgs could contain single value "Frodo"
+                // 5 => sortOrder = string containing names of the fields you want the data sorted by. (ORDER BY clause)
 
-            val contacts = ArrayList<String>() // create a list to hold our contacts.
-            cursor?.use {
-                // loop through the cursor
-                while (it.moveToNext()) {
-                    contacts.add(it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)))
+                val contacts = ArrayList<String>() // create a list to hold our contacts.
+                cursor?.use {
+                    // loop through the cursor
+                    while (it.moveToNext()) {
+                        contacts.add(it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)))
+                    }
+
+                    val adapter =
+                        ArrayAdapter<String>(this, R.layout.contact_detail, R.id.name, contacts)
+                    contactNames.adapter = adapter
                 }
+            } else {
+                Snackbar.make(view, "Please grant access to your Contacts", Snackbar.LENGTH_LONG).setAction("Action", null)
+                    .show()
             }
 
-            val adapter = ArrayAdapter<String>(this, R.layout.contact_detail, R.id.name, contacts)
-            contact_names.adapter = adapter
 
             Log.d(TAG, "onCreate: onclick => ends")
         }
@@ -101,6 +110,7 @@ class MainActivity : AppCompatActivity() {
                         Log.d(TAG, "onRequestPermissionsResult: permission refused")
                         false
                     }
+//                fab.isEnabled = readGranted
             }
         }
         Log.d(TAG, "onRequestPermissionsResult: ends")
